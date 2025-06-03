c079c54bffdc57221b6fe804c4bfc24004654e48
Maitri Sarif <maitrisarif@pop-os.localdomain>
2025-06-03 15:03:59 -0400

    added personal highscore

--- src/main.js
+++ src/main.js
@@ -109,7 +109,12 @@ scene("game", () => {
 
     // wanted milk box
     const match = add([
-        choose([sprite("yellowmilk"), sprite("cyanmilk"), sprite("magentamilk"), sprite("keymilk")]),
+        choose([
+            sprite("yellowmilk"), 
+            sprite("cyanmilk"), 
+            sprite("magentamilk"), 
+            sprite("keymilk")
+        ]),
         pos(340,24),
         "match"
     ])
@@ -126,7 +131,12 @@ scene("game", () => {
     function spawnBox() {
         max -= 0.1;
         const box = add([
-            choose([sprite("yellowmilk"), sprite("cyanmilk"), sprite("magentamilk"), sprite("keymilk")]),
+            choose([
+                sprite("yellowmilk"), 
+                sprite("cyanmilk"), 
+                sprite("magentamilk"), 
+                sprite("keymilk")
+            ]),
             area(),
             pos(width(), height() - 48),
             anchor("botleft"),
@@ -177,7 +187,13 @@ scene("game", () => {
             shake();
             isNowBeef = true;
             wait(0.8, () => {
-                go("lose", score);
+                // loser
+                if (localStorage.getItem("user")){
+                    go("lose", score);
+                } else {
+                    go("username",score)
+                }
+                
             });
         }
     });
@@ -188,27 +204,54 @@ scene("game", () => {
     });
 })
 
+scene("username", (score) => {
+
+    // check if localStorage.getItem("user") is true or not in game scene
+    let username = localStorage.getItem("user");
+    add([ sprite("plainbackground"), pos(0, 0),]);
+    add([text("Type your name and press enter"), pos(width()/2,height()/2 - 50), scale(0.5), anchor("center"), color(0,0,0)]);
+    const obj = add([   
+        text("______"),
+        textInput(),
+        pos(width()/2, height()/2),
+        anchor("center"),
+        scale(0.5),
+    ])
+
+    obj.hasFocus = true
+    obj.onInput(()=>{
+        console.log(obj.typedText)
+        localStorage.setItem("user", obj.typedText)
+        username = obj.typedText
+    })
+
+    // add([text(username), pos(width()/2,60), scale(1.3), anchor("center"), color(0,0,0)]);
+    onKeyPress("enter", () => go("lose",score))
+
+})
 
 scene("lose", (score) => {
     // localstorage here
-    let highscore = localStorage.getItem("hs");
+    let highscore = localStorage.getItem("high");
     if (highscore && score > highscore) {
-        localStorage.setItem("hs", score);
+        localStorage.setItem("high", score);
         highscore = score;
     }
 
     if(!highscore) {
-        localStorage.setItem("hs", score);
+        localStorage.setItem("high", score);
         highscore = score;
     }
-    
+    console.log(highscore)
     
 
     add([ sprite("plainbackground"), pos(0, 0),]);
-    add([text("Game Over"), pos(center()), scale(1.3), anchor("center"), color(255,255,219)]);
-    add([text("Score: " + score + "   Highscore: " + (highscore || 0)), pos(width() / 2, height() / 2 + 80), anchor("center"), scale(0.6), color(255,255,219)]);
+    add([text("Game Over"), pos(width()/2,30), scale(1.3), anchor("center"), color(0,0,0)]);
+    add([text("Highscores"), pos(width() / 2, 80), anchor("center"), scale(0.6), color(0,0,0)]);
+    add([text(localStorage.getItem("user") + " - " + (highscore || 0)), pos(width() / 2, 120), anchor("center"), scale(0.6), color(50,50,50)]);
 
-    add([text("Click to Restart"), pos(width() / 2, height() / 2 + 120), anchor("center"), scale(0.6), color(255,255,219)]);
+    add([text("Score: " + score ), pos(width() / 2, height() / 2 + 150), anchor("center"), scale(0.6), color(255,255,219)]);
+    add([text("Click to Restart"), pos(width() / 2, height() / 2 + 180), anchor("center"), scale(0.6), color(255,255,219)]);
     onClick(() => go("game"));
     onKeyPress("space", () => go("game")); 
 });
